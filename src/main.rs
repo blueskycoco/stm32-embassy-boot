44c068fc0ceb5ef4781a55b9adae3adb00653363
Dillon Min <dillon.min@780usff.com>
2025-09-11 11:22:10 +0800

    fix compile warnings
    Signed-off-by: Dillon Min <dillon.min@780usff.com>

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,16 @@ use embassy_stm32::time::Hertz;
 use embassy_stm32::flash::{Flash, BANK1_REGION, WRITE_SIZE};
 //use embassy_stm32::rcc::WPAN_DEFAULT;
 use embassy_stm32::usb::Driver;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Speed};
+use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_stm32::{bind_interrupts, peripherals, usb};
 use embassy_sync::blocking_mutex::Mutex;
-use embassy_usb::{msos, Builder};
+use embassy_usb::{Builder};
 use embassy_usb_dfu::consts::DfuAttributes;
 use embassy_usb_dfu::{usb_dfu, Control, ResetImmediate};
 
 bind_interrupts!(struct Irqs {
     OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
 });
-
-// This is a randomly generated GUID to allow clients on Windows to find your device.
-//
-// N.B. update to a custom GUID for your own device!
-const DEVICE_INTERFACE_GUIDS: &[&str] = &["{EAA9A5DC-30BA-44BC-9232-606CDC875321}"];
 
 // This is a randomly generated example key.
 //
@@ -64,7 +59,8 @@ fn main() -> ! {
     let flash = Mutex::new(RefCell::new(layout.bank1_region));
     let flash2 = Mutex::new(RefCell::new(layout.bank2_region));
 
-    let config = BootLoaderConfig::from_linkerfile_blocking(&flash, &flash2, &flash);
+    let config = BootLoaderConfig::from_linkerfile_blocking(&flash, &flash2,
+                                                            &flash);
     let active_offset = config.active.offset();
     let mut led = Output::new(p.PC13, Level::High, Speed::Low);
     let bl = BootLoader::prepare::<_, _, _, 2048>(config);
@@ -74,13 +70,15 @@ fn main() -> ! {
         let mut usb_config = embassy_stm32::usb::Config::default();
         usb_config.vbus_detection = false;
         let mut ep_out_buffer = [0u8; 256];
-        let driver = Driver::new_fs(p.USB_OTG_FS, Irqs, p.PA12, p.PA11, &mut ep_out_buffer, usb_config);
+        let driver = Driver::new_fs(p.USB_OTG_FS, Irqs, p.PA12, p.PA11,
+                                    &mut ep_out_buffer, usb_config);
         let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
         config.manufacturer = Some("Embassy");
         config.product = Some("USB-DFU Bootloader example");
         config.serial_number = Some("1235678");
 
-        let fw_config = FirmwareUpdaterConfig::from_linkerfile_blocking(&flash2, &flash);
+        let fw_config = FirmwareUpdaterConfig::from_linkerfile_blocking(&flash2,
+                                                                        &flash);
         let mut buffer = AlignedBuffer([0; WRITE_SIZE]);
         let updater = BlockingFirmwareUpdater::new(fw_config, &mut buffer.0[..]);
 
@@ -89,12 +87,14 @@ fn main() -> ! {
         let mut control_buf = [0; 2048];
 
         #[cfg(not(feature = "verify"))]
-        let mut state = Control::new(updater, DfuAttributes::CAN_DOWNLOAD, ResetImmediate);
+        let mut state = Control::new(updater, DfuAttributes::CAN_DOWNLOAD,
+                                        ResetImmediate);
 
         #[cfg(feature = "verify")]
-        let mut state = Control::new(updater, DfuAttributes::CAN_DOWNLOAD, ResetImmediate, PUBLIC_SIGNING_KEY);
+        let mut state = Control::new(updater, DfuAttributes::CAN_DOWNLOAD,
+                                        ResetImmediate, PUBLIC_SIGNING_KEY);
 
-    led.set_low();
+        led.set_low();
         let mut builder = Builder::new(
             driver,
             config,
@@ -104,28 +104,7 @@ fn main() -> ! {
             &mut control_buf,
         );
 
-        // We add MSOS headers so that the device automatically gets assigned the WinUSB driver on Windows.
-        // Otherwise users need to do this manually using a tool like Zadig.
-        //
-        // It seems these always need to be at added at the device level for this to work and for
-        // composite devices they also need to be added on the function level (as shown later).
-        //
-     //   builder.msos_descriptor(msos::windows_version::WIN8_1, 2);
-     //   builder.msos_feature(msos::CompatibleIdFeatureDescriptor::new("WINUSB", ""));
-     //   builder.msos_feature(msos::RegistryPropertyFeatureDescriptor::new(
-     //       "DeviceInterfaceGUIDs",
-     //       msos::PropertyData::RegMultiSz(DEVICE_INTERFACE_GUIDS),
-     //   ));
-
-        usb_dfu::<_, _, _, _, 2048>(&mut builder, &mut state, |func| {
-            // You likely don't have to add these function level headers if your USB device is not composite
-            // (i.e. if your device does not expose another interface in addition to DFU)
-     //       func.msos_feature(msos::CompatibleIdFeatureDescriptor::new("WINUSB", ""));
-     //       func.msos_feature(msos::RegistryPropertyFeatureDescriptor::new(
-     //           "DeviceInterfaceGUIDs",
-     //           msos::PropertyData::RegMultiSz(DEVICE_INTERFACE_GUIDS),
-       //     ));
-        });
+        usb_dfu::<_, _, _, _, 2048>(&mut builder, &mut state, |_func| {});
 
         let mut dev = builder.build();
         embassy_futures::block_on(dev.run());
